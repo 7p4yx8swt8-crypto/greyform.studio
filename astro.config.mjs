37e5fae4020d1151a1e0a0a6e27b9b06e8c1f608
Patrik Salewski <7p4yx8swt8@privaterelay.appleid.com>
2026-05-30 22:30:04 +0200

    feat: add sitemap plugin + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://greyform.studio",
+
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "de",
+        locales: {
+          de: "de-DE",
+          en: "en-US",
+        },
+      },
+    }),
+  ],
 
   markdown: {
     shikiConfig: {
       theme: "min-light",
     },
-    // Allow HTML inside markdown (e.g. for custom row layouts)
     smartypants: true,
   },
 
